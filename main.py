f0ba129990f53d5d5fa18d279357ee1a67c0f193
ranchaya sambath <ranchayasambath@gmail.com>
2022-06-03 23:45:08 -0500

    found solution for checkbox for email copy

--- main.py
+++ main.py
@@ -7,14 +7,13 @@ PATH = "C:\chromedriver.exe"
 
 # Set chromedriver's path
 driver = webdriver.Chrome(PATH)
-
+driver.maximize_window() # For maximizing window
+driver.implicitly_wait(20) # gives an implicit wait for 20 seconds
 # Store url inside variable
 url = 'https://share.hsforms.com/1DXiqHMY_Sie_ud0bcep1VQ1odv5?__hstc=241689437.dd647fa92316e2c182cc877f6b41a72b.1552417543081.1631030947093.1631039819157.252&__hssc=241689437.1.1631039819157&__hsfp=988172843&utm_medium=email&_hsmi=156987797&_hsenc=p2ANqtz-9DRFQ53QBgBj-SF2JxoYDHPadh6Xo04fjwykDHB6eBtFg3a1N5BIwinDQCB_zHGdMfCK3OF2trzIFLqou9ou3sPyjDzySx5QQ7j6Lx8SZ7N6id0Gk&utm_content=156987797&utm_source=hs_email'
 
 #Initial page load
 # driver.get(url)
-driver.maximize_window() # For maximizing window
-driver.implicitly_wait(20) # gives an implicit wait for 20 seconds
 
  #Identify user input fields through id's
 # input_fname = driver.find_element_by_id('firstname-input')
@@ -27,8 +26,6 @@ driver.implicitly_wait(20) # gives an implicit wait for 20 seconds
 
 # input_job_source = driver.find_element_by_id('job_source_-input')
 
-# # input_check_email = driver.find_element_by_id('copy_of_form_submission-input')
-
 # # automated information filling out
 # input_fname.send_keys('Ranchaya')
 
@@ -40,9 +37,10 @@ driver.implicitly_wait(20) # gives an implicit wait for 20 seconds
 
 # input_job_source.send_keys('Independent Search')
 
-
-# # if not input_check_email.is_selected:
-#     # input_check_email.send_keys(value='true')
+# #checkbox for email copy
+# agreed = driver.find_element_by_id("copy_of_form_submission-input")
+# if agreed.get_attribute("checked") != "true":
+#     agreed.click()
 
 # # Consecutive tabs
 # driver.execute_script("window.open('');")
@@ -177,5 +175,11 @@ for i in range (5):
     input_cohort.send_keys('Sirius')
 
     input_job_source.send_keys('Independent Search')
+
+    agreed = driver.find_element_by_id("copy_of_form_submission-input")
+    if agreed.get_attribute("checked") != "true":
+        agreed.click()
+
+ #Terminate all chromedrivers that built up to reclaim resources.   
 subprocess.call("TASKKILL /f  /IM  CHROMEDRIVER.EXE")
-#----------------------------------------------------------------------------------
+#--------------------------------------------------------------------------------------------
